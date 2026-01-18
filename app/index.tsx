4bb41f8ddfbcd9865c13789a08419671f9da502b
Enric Velasco <30808265+enricvelasco@users.noreply.github.com>
2026-01-18 23:49:38 +0100

    create home animated menu with geometry wrappers (#2)
    Co-authored-by: Enric Velasco Garrofé <evelasco@alfredsmart.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,46 @@
 import {Canvas} from '@react-three/fiber/native'
 import { OrbitControls } from '@react-three/drei/native'
-import {CapsuleGeometry} from "@/components/common/capsuleGeometry";
+import {GeometryWrapper} from "@/components/common/geometryWrapper";
+import {BoxGeometry} from "@/components/common/geometryElements/boxGeometry";
+import {Vector3} from "three";
+import {useCameraZoomToTarget} from "@/hooks/useCameraZoomTarget";
+
+export const Scene = () => {
+  const { onSetTarget } = useCameraZoomToTarget(new Vector3(0, 0, 5));
+  return (
+      <>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[-5, 5, 5]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <group>
+          <GeometryWrapper position={[0, 0, 0]} onClick={onSetTarget}>
+            <BoxGeometry
+                color={'red'}
+                size={{width: 1.5, height: 2.5, depth: 0.1}}
+            />
+          </GeometryWrapper>
+          <GeometryWrapper position={[-1.5, 0, -1.5]} onClick={onSetTarget}>
+            <BoxGeometry
+                color={'blue'}
+                size={{width: 1.5, height: 2.5, depth: 0.1}}
+            />
+          </GeometryWrapper>
+          <GeometryWrapper position={[1.5, 0, -1.5]} onClick={onSetTarget}>
+            <BoxGeometry
+                color={'yellow'}
+                size={{width: 1.5, height: 2.5, depth: 0.1}}
+            />
+          </GeometryWrapper>
+        </group>
+        <OrbitControls />
+      </>
+  );
+}
 
 export default function HomeScreen() {
   return (
       <>
         <Canvas>
-          <ambientLight intensity={0.2} />
-          <directionalLight
-              color="white"
-              intensity={1}
-              position={[-5, 5, 5]}
-          />
-          <OrbitControls position0={[0, 0, 0]} />
-          <CapsuleGeometry
-              position={[0, 0, 0]}
-              size={{
-                radius: 1,
-                height: 1,
-                capSegments: 10,
-                radialSegments: 20,
-                heightSegments: 1,
-              }}
-          />  {/* capsula */}
+          <Scene />
         </Canvas>
       </>
   );
