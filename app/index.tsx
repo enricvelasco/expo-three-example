5fdcfbc06487347c3601189296c3faf08e1d468b
Enric Velasco Garrofé <evelasco@alfredsmart.com>
2026-01-18 12:02:26 +0100

    first 3D model, with asset

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,32 @@
-import {Text} from "react-native";
+import { Suspense } from 'react'
+import { Canvas, ThreeElements } from '@react-three/fiber/native'
+import { useGLTF, OrbitControls } from '@react-three/drei/native'
+import modelPath from '../assets/glb/blueBook.glb'
+
+type ModelProps = {
+  position?: [number, number, number];
+  rotation?: [number, number, number];
+  scale?: number | [number, number, number];
+}
+
+function Model(props: ModelProps) {
+  const gltf = useGLTF(modelPath)
+  return <primitive {...props} object={gltf.scene} />
+}
 
 export default function HomeScreen() {
-  return <Text>Home</Text>;
+  return (
+      <Canvas>
+        <ambientLight intensity={2} />
+        {/*<directionalLight position={[10, 10, 5]} intensity={1} />*/}
+        <OrbitControls />
+        <Suspense>
+          <Model
+              scale={1}
+              position={[0, 0, 0]}
+              rotation={[0.3, 0.4, 0]}
+          />
+        </Suspense>
+      </Canvas>
+  );
 }
